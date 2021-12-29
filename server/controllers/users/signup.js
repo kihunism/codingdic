df5d1a3e0f6eab011fdd772d7314a8974d83b9f3
kihunism <resacle93@naver.com>
2021-12-29 17:44:33 +0900

    main branch test

--- server/controllers/users/signup.js
+++ server/controllers/users/signup.js
@@ -3,12 +3,6 @@ const { user } = require('../../models')
 module.exports = async (req, res) => {
     const { email, password, password_check, nickname } = req.body
 
-    await user.create({
-        email: email,
-        password: password,
-        nickname: nickname
-    })
-
     res.status(201).send({
         data: {
             email,
